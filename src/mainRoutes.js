5e873ebce686bfa2a11e6fdfd9e754661669aed8
Mohammed Alli Tar-Mahomed <malitm10@gmail.com>
2023-05-09 12:08:25 +0200

    Create Express and add Main Routes

---
+++ src/mainRoutes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World. I\'m a Node app.')
+})
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+module.exports = mainRouter
