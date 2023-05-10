fa1922f2ff3473ffc50a148f78853bb11d044663
Miguel Hunter <miguelhunter0124@gmail.com>
2023-05-10 13:54:22 +0200

    Added calendars to dashboards

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -7,6 +7,6 @@ const app = express()
 const mainRouter = require('./mainRoutes.js')
 
 app.use('/', mainRouter)
-app.use('/cdn', express.static('src/public'))
+
 app.listen(3000)
 console.log('Express server running on port 3000')
