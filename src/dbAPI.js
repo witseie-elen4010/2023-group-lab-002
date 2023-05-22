263c93a301b1d6843606cb522161749217054c55
Miguel Hunter <miguelhunter0124@gmail.com>
2023-05-22 13:06:19 +0200

    Set availabilty and fixed format npm standard

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -5,73 +5,81 @@ const dbAPI = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
-mongoose.connect("mongodb+srv://consultation:Em73gnZ6CfElljU1@consultation-web.mp2vkpc.mongodb.net/consultation?retryWrites=true&w=majority")
+mongoose.connect('mongodb+srv://consultation:Em73gnZ6CfElljU1@consultation-web.mp2vkpc.mongodb.net/consultation?retryWrites=true&w=majority')
 const userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    type: String,
-    name: String,
-    email: String
+  username: String,
+  password: String,
+  type: String,
+  name: String,
+  email: String,
+  day: [Number],
+  time: [String],
+  duration: [Number]
 })
 const User = mongoose.model('User', userSchema)
 
 dbAPI.post('/login', async function (req, res) {
-    const user = await User.findOne({username: req.body.username})
-    if (!user){
-        req.session.errorLogin = true
-        res.redirect('/login')
+  const user = await User.findOne({ username: req.body.username })
+  if (!user) {
+    req.session.errorLogin = true
+    res.redirect('/login')
+  } else if (bcrypt.compareSync(req.body.password, user.password)) {
+    req.session.user = user
+    if (user.type === 'lecturer') {
+      res.redirect('/lecturerDashboard')
+    } else {
+      res.redirect('/studentdashboard')
     }
-    else if (bcrypt.compareSync(req.body.password, user.password)) {
-        req.session.user = user
-        if(user.type == 'lecturer'){
-            res.redirect('/lecturerDashboard')
-        }
-        else{
-            res.redirect('/studentdashboard')
-        }
-    }
-    else {
-        req.session.errorLogin = true
-        res.redirect('/login')
-    }
+  } else {
+    req.session.errorLogin = true
+    res.redirect('/login')
+  }
 })
 
 dbAPI.get('/incorrectLogin', function (req, res) {
-    res.send(req.session.errorLogin)
+  res.send(req.session.errorLogin)
 })
 
-dbAPI.get('/checkUsername/:username', async function(req, res){
-    const user = await User.findOne({username: req.params.username})
-    if (user){
-        res.send('true')
-    }
-    else{
-        res.send('false')       
-    }
+dbAPI.get('/checkUsername/:username', async function (req, res) {
+  const user = await User.findOne({ username: req.params.username })
+  if (user) {
+    res.send('true')
+  } else {
+    res.send('false')
+  }
 })
 
-dbAPI.get('/checkEmail/:email', async function(req, res){
-    const user = await User.findOne({email: req.params.email})
-    if (user){
-        res.send('true')
-    }
-    else{
-        res.send('false')
-    }
+dbAPI.get('/checkEmail/:email', async function (req, res) {
+  const user = await User.findOne({ email: req.params.email })
+  if (user) {
+    res.send('true')
+  } else {
+    res.send('false')
+  }
 })
 
 dbAPI.post('/signup', async function (req, res) {
-    const hashPassword = await bcrypt.hash(req.body.password,12)
-    const newUser = new User({ name: req.body.name, username: req.body.username, email: req.body.email, password: hashPassword, type: req.body.position})
-    await newUser.save()
-    req.session.user = newUser
-    res.redirect('/dashboard')
+  const hashPassword = await bcrypt.hash(req.body.password, 12)
+  const newUser = new User({ name: req.body.name, username: req.body.username, email: req.body.email, password: hashPassword, type: req.body.position })
+  await newUser.save()
+  req.session.user = newUser
+  res.redirect('/dashboard')
 })
 
-//Use later for deleting existing accounts
-dbAPI.post('/delete', async function(req, res){
-    await User.deleteOne({username: req.body.username})
-    res.redirect('/login')
+// Use later for deleting existing accounts
+dbAPI.post('/delete', async function (req, res) {
+  await User.deleteOne({ username: req.body.username })
+  res.redirect('/login')
+})
+
+dbAPI.post('/setAvailability', async function (req, res) {
+  const username = req.session.user.username
+  const lecturer = await User.findOne({ username })
+  lecturer.day.push(req.body.day)
+  lecturer.time.push(req.body.time)
+  lecturer.duration.push(req.body.duration)
+  await lecturer.save()
+  res.redirect('/lecturerDashboard')
 })
 
 module.exports = dbAPI
