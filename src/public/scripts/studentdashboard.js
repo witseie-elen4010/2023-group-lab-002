d9ccc5afd5695631af2d4b7bf4caa0d6c81aa8ef
Priyanka Kala <1146648@students.wits.ac.za>
2023-05-24 13:15:16 +0200

    Access lecturer available times

--- src/public/scripts/studentdashboard.js
+++ src/public/scripts/studentdashboard.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const lecturer = document.getElementById('lecturerDropdown')
+const lecturerDropdown = document.getElementById('lecturerDropdown')
 const scheduleDiv = document.getElementById('scheduleDiv')
+let allLecturers
 
 function addLecturer (dbLecturer) {
   const option = document.createElement('option')
   option.setAttribute('value', dbLecturer.username)
   const optionText = document.createTextNode(dbLecturer.name)
   option.appendChild(optionText)
-  lecturer.appendChild(option)
+  lecturerDropdown.appendChild(option)
 }
 
 fetch('/db/getLecturers')
@@ -18,21 +19,68 @@ fetch('/db/getLecturers')
     }
   })
   .then(data => {
+    allLecturers = data
     data.forEach(addLecturer)
   })
-lecturer.addEventListener('change', function (Event) {
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+lecturerDropdown.addEventListener('change', function (Event) {
+  let lecturer = null
+  let i = 0
+  while (lecturer === null) {
+    if (allLecturers[i].username === lecturerDropdown.value) {
+      lecturer = allLecturers[i]
+    }
+    i++
+  }
   if (scheduleDiv.hasChildNodes('table')) {
     scheduleDiv.removeChild(scheduleDiv.lastChild)
   }
   const table = document.createElement('table')
   table.classList.add('table', 'table-bordered')
-  const row = table.insertRow()
-  const row2 = table.insertRow()
-  const cell = row.insertCell()
-  const cell2 = row2.insertCell()
-
+  const tableHeader = table.insertRow()
+  const cell = tableHeader.insertCell()
   cell.textContent = 'Available times'
-  cell2.textContent = 'Monday 3-4 pm'
 
+  const columnNames = table.insertRow()
+  const cell1 = columnNames.insertCell()
+  const cell2 = columnNames.insertCell()
+  const cell3 = columnNames.insertCell()
+  const cell4 = columnNames.insertCell()
+
+  cell1.textContent = 'Day'
+  cell2.textContent = 'Time'
+  cell3.textContent = 'Duration (min)'
+  cell4.textContent = 'Group Size'
+
+  table.appendChild(tableHeader)
+  table.appendChild(columnNames)
+
+  // Collect table rows into an array
+  const rows = []
+  for (let i = 0; i < lecturer.day.length; i++) {
+    const row = table.insertRow()
+    const cell1 = row.insertCell()
+    cell1.textContent = days[lecturer.day[i] - 1]
+    const cell2 = row.insertCell()
+    cell2.textContent = lecturer.time[i]
+    const cell3 = row.insertCell()
+    cell3.textContent = lecturer.duration[i]
+    const cell4 = row.insertCell()
+    cell4.textContent = lecturer.groupSize[i]
+    rows.push(row)
+  }
+  rows.sort((a, b) => {
+    const dayA = days.indexOf(a.cells[0].textContent) + 1
+    const dayB = days.indexOf(b.cells[0].textContent) + 1
+    return dayA - dayB
+  })
+  while (table.rows.length > 2) {
+    table.deleteRow(2)
+  }
+  for (let i = 0; i < rows.length; i++) {
+    table.appendChild(rows[i])
+  }
   scheduleDiv.appendChild(table)
 })
